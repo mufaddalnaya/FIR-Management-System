e43c074bd4b83dc560496259d0a7932d39b3ced7
Mufaddal Naya <mufaddal.naya@gmail.com>
2019-07-23 19:10:14 +0530

    Newly Added

---
+++ MN FIR MANAGEMENT SYSTEM/src/mn_fir_management_system/MN_FIR_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mn_fir_management_system;
+
+/**
+ *
+ * @author hp
+ */
+public class MN_FIR_MANAGEMENT_SYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
